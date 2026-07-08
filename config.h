890c3f21bc47d7fc152f980ca74342a8b13184f7
LeeNIX <leebrandt@pm.me>
2026-07-08 00:55:30 -0500

    Small moves, Ellie.

--- config.h
+++ config.h
@@ -61,13 +61,15 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-l", "20", NULL };
+static const char *appcmd[] = {"/home/lee/.local/bin/dmenu_apps.sh", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const char *browsercmd[] = { "brave", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_p,  	   spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_space,  spawn,          {.v = appcmd } },
 	{ MODKEY,             		XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
